0fc55dd3f1c7fc788bb513943d689f02eb7d6d97
Karinacc <872974856@qq.com>
2022-12-31 12:10:27 +0800

    Create 2212.sql

--- SQL/2212.sql
+++ SQL/2212.sql
@@ -385,3 +385,29 @@ WHERE Id IN
       );
     END
 
+
+#22.12.31
+
+#法一：经典常规开窗
+
+    SELECT 
+        c.name customer_name,
+        c.customer_id, 
+        t.order_id, 
+        t.order_date
+    FROM (
+        SELECT  
+            order_id,
+            order_date,
+            customer_id,
+            row_number() over(partition by customer_id order by order_date DESC) as rank_tag
+        FROM orders ) t
+    LEFT JOIN customers c  
+        ON t.customer_id = c.customer_id    
+    WHERE 
+        rank_tag <= 3
+    ORDER BY 
+        name, customer_id, order_date DESC;
+
+
+
