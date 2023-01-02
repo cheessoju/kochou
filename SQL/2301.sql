128fcd5c5fce97893cc3bdea8a7f5643a3c5193b
Karinacc <872974856@qq.com>
2023-01-02 23:40:06 +0800

    Update 2301.sql

--- SQL/2301.sql
+++ SQL/2301.sql
@@ -14,3 +14,27 @@
 
     SELECT user1_id, user2_id FROM temp
     WHERE k = (SELECT MAX(k) FROM temp)
+
+#2023.1.2
+#今天做不动了其实
+#法一
+    SELECT gender, day, 
+    SUM(score_points) OVER (PARTITION BY gender ORDER BY day) total
+FROM scores
+
+#法二
+#递归子集的非等值自连接法
+    SELECT
+        t1.gender,
+        t1.day,
+        (
+            SELECT
+                SUM(t2.score_points)
+            FROM
+                Scores AS t2
+            WHERE t1.day >= t2.day
+            AND t1.gender = t2.gender
+        ) AS 'total'
+    FROM
+        Scores AS t1
+    ORDER BY gender, day
